ccedb470cef71b2def1fce8e981cb2ff93daa737
Jeremy Fairbank <elpapapollo@gmail.com>
2014-06-26 14:27:58 -0400

    Rename Dims -> Measurb. Remove singleton method on 'Measurb' too.

--- spec/helpers/clear_dimensions.rb
+++ spec/helpers/clear_dimensions.rb
@@ -1,14 +1,16 @@
 def remove_loaded_default_dimensions
   $".delete_if do |file|
-    file =~ %r{dims-rb/lib/dims/defaults}
+    file =~ %r{measurb/lib/measurb/defaults}
   end
 end
 
 def clear_defined_dimension(name, abbrev)
   class_name = name.to_s.capitalize
-  Dims.__send__(:remove_const, class_name) if Dims.const_defined?(class_name)
-  Numeric.__send__(:undef_method, name)    if Numeric.method_defined?(name)
-  Numeric.__send__(:undef_method, abbrev)  if Numeric.method_defined?(abbrev)
+
+  Measurb.singleton_class.__send__(:remove_method, name) if Measurb.singleton_class.method_defined?(name)
+  Measurb.__send__(:remove_const, class_name)            if Measurb.const_defined?(class_name)
+  Numeric.__send__(:remove_method, name)                 if Numeric.method_defined?(name)
+  Numeric.__send__(:remove_method, abbrev)               if Numeric.method_defined?(abbrev)
 end
 
 def clear_defined_inches
