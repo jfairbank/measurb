13b5963ba3563f22bdd2d3d9501f2acca7df16d3
Jeremy Fairbank <elpapapollo@gmail.com>
2014-06-26 14:28:18 -0400

    Rename Dims -> Measurb

--- spec/numeric_ext_spec.rb
+++ spec/numeric_ext_spec.rb
@@ -4,19 +4,19 @@ describe Numeric do
   describe '#inches' do
     before :context do
       clear_defined_inches
-      Dims.define(:inches)
+      Measurb.define(:inches)
     end
 
     shared_context 'returning expected' do |n|
       it 'returns the dimension class instance' do
-        expect(n.inches).to be_a(Dims::Inches)
-        expect(n.inches).to eq(Dims::Inches.new(n))
+        expect(n.inches).to be_a(Measurb::Inches)
+        expect(n.inches).to eq(Measurb::Inches.new(n))
       end
     end
 
     shared_context 'having correct precision' do |n|
       it 'sets default precision without an argument' do
-        expect(n.inches).to have_precision(Dims::DEFAULT_PRECISION)
+        expect(n.inches).to have_precision(Measurb::DEFAULT_PRECISION)
       end
 
       it 'sets the supplied precision' do
